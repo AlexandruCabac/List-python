40b4244ae18c3f7a1540c95375966e4dcdd058a7
AlexandruCabac <70658170+AlexandruCabac@users.noreply.github.com>
2021-02-02 11:05:12 +0200

    Add files via upload

--- Pr1.py
+++ Pr1.py
@@ -1,17 +1,12 @@
-a=b=c=d=[]
-a.extend(input().split())
-for i in range(0, len(a)):
-    a[i]=int(a[i])
-b=sorted(a)
-c=sorted(a,reverse=True)
-d=a+[111]
-print(a, b, c, len(a), max(a), min(a), d, sep='\n')
-a.insert(1,222)
-print(a)
-#a=[7,-3,4,1,0-8,-11123,3412]
-#b=sorted(a)
-#c=sorted(a,reverse=True)
-#d=a+[111]
-#print(a, b, c, len(a), max(a), min(a), d, sep='\n')
-#a.insert(1,222)
-#print(a)
+i=p=0
+with open('input.txt','r')as f:
+    a=list(f.readline())
+for k in range(len(a)):
+    if(int(a[k])==0 or int(a[k])%2==0):
+        p+=1
+    else:
+        i+=1
+with open('output.txt','w')as f:
+    c1="Numarul cifrelor impare sunt"
+    c2="Numarul cifrelor pare sunt"
+    f.write("{0}{1}{2}{3}{4}".format(c1,i,'\n',c2,p))
--- Pr2.py
+++ Pr2.py
@@ -1,12 +1,16 @@
-a=b=c=d=[]
+b=0
 with open('input.txt','r')as f:
-    a.extend(f.readline().split())
-for i in range(0, len(a)):
-    a[i]=int(a[i])
-b=sorted(a)
-c=sorted(a,reverse=True)
-d=a+[111]
+    a=list(f.readline())
+for k in range(len(a)-1):
+    if(int(a[k])<int(a[k+1])):
+        b=1
+    else:
+        b=0
+        break
 with open('output.txt','w')as f:
-    f.write("{0}{7}{1}{7}{2}{7}{3}{7}{4}{7}{5}{7}{6}{7}".format(a, b, c, len(a), max(a), min(a), d,'\n'))
-    a.insert(1,222)
-    f.write("{}".format(a))
+    c1="Da"
+    c2="Nu"
+    if(b==0):
+        f.write(c2)
+    else:
+        f.write(c1)
